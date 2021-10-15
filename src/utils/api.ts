695b25d448d5189d11cebd9593ecd4542aee28ad
Gabriel Ferreira <gabriel.ferreira@arquivei.com.br>
2021-10-14 22:22:14 -0300

    Typescript Improvements (#20)
    * refactor: optimize and types
    
    - fix chalk type import
    - optimize fetch object request
    
    * fix: correct path to get types
    
    * chore: dependencies ts
    
    - removed tslib
    - added ts-node to run dev mode
    - added scripts
    - added typechecking
    - move types to correct path

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,8 +1,18 @@
 import axios from 'axios';
-import type { CorreiosError } from './';
+import type { CorreiosError, CorreiosResponse } from '@types';
 
-const baseURL = 'https://rastreamento.correios.com.br/app/';
-export const api = axios.create({ baseURL });
+const baseURL = 'https://rastreamento.correios.com.br/app/resultado.php';
+
+export const fetchObject = async (code: string) => {
+	const { data } = await axios.get<CorreiosResponse>(baseURL, {
+		params: {
+			objeto: code,
+			mqs: 'S',
+		},
+	});
+
+	return data;
+};
 
 export function isError(data: any): data is CorreiosError {
 	return data.erro !== undefined;
