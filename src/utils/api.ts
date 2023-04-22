4c581dc21537b1b7cc9da78ff175b7ac853e3700
Mauricio Mutte <mauriciomutte.mm@gmail.com>
2023-04-22 13:35:32 -0300

    fix: correct enum notation

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
+
 import type { CorreiosError, CorreiosResponse } from '@types';
 
-const baseURL = 'https://proxyapp.correios.com.br/v1/sro-rastro';
+const BASE_URL = 'https://proxyapp.correios.com.br/v1/sro-rastro';
 
 export const fetchObject = async (code: string) => {
-	const { data } = await axios.get<CorreiosResponse>(`${baseURL}/${code}`);
+	const { data } = await axios.get<CorreiosResponse>(`${BASE_URL}/${code}`);
 
 	return data;
 };
